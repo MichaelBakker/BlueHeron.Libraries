295dba9dbe09aa1cab6219556144ee9e243194e8
Michael Bakker <michaelbakker1@gmail.com>
2023-01-01 12:13:43 +0100

    Add extension method to enable sorting of Observablecollections without breaking any bindings.

--- BlueHeron.Collections/BlueHeron.Collections/Extensions.cs
+++ BlueHeron.Collections/BlueHeron.Collections/Extensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using BlueHeron.Collections.Generic;
 
@@ -30,6 +32,27 @@ namespace BlueHeron.Collections
 			else
 			{
 				return list.ToArray();
+			}
+		}
+
+		#endregion
+
+		#region ObservableCollections
+
+		/// <summary>
+		/// Sorts this <see cref="ObservableCollection{T}"/> without breaking any bindings.
+		/// </summary>
+		/// <typeparam name="T">The <see cref="Type"/> of the element in the collection</typeparam>
+		/// <param name="collection">The <see cref="ObservableCollection{T}"/> to sort</param>
+		/// <param name="comparison">The <see cref="Comparison{T}"/> to use when sorting the items in the collection</param>
+		public static void Sort<T>(this ObservableCollection<T> collection, Comparison<T> comparison)
+		{
+			var sortableList = new List<T>(collection);
+			sortableList.Sort(comparison);
+
+			for (int i = 0; i < sortableList.Count; i++)
+			{
+				collection.Move(collection.IndexOf(sortableList[i]), i);
 			}
 		}
 
