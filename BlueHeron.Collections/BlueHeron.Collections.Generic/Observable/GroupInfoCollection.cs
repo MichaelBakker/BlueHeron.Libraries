24c727b22b5a08bac3651a5db9321fd6d25ba5f0
Michael Bakker <michaelbakker1@gmail.com>
2023-02-11 08:51:34 +0100

    Add Singleton container. Add GroupInfoCollection.

---
+++ BlueHeron.Collections/BlueHeron.Collections.Generic/Observable/GroupInfoCollection.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace BlueHeron.Collections.Generic;
+
+/// <summary>
+/// An <see cref="ObservableCollection{T}"/> with a key property, useful in grouping operations.
+/// </summary>
+/// <typeparam name="T">The <see cref="System.Type"/> of the elements in the collection</typeparam>
+public class GroupInfoCollection<T> : ObservableCollection<T>
+{
+    /// <summary>
+    /// The key of this collection.
+    /// </summary>
+    public object Key { get; set; }
+
+    /// <summary>
+    /// Returns the <see cref="IEnumerator{T}"/> for this collection.
+    /// </summary>
+    /// <returns>An <see cref="IEnumerator{T}"/></returns>
+    public new IEnumerator<T> GetEnumerator() => (IEnumerator<T>)base.GetEnumerator();
+}
---
+++ BlueHeron.Collections/BlueHeron.Collections.Generic/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace BlueHeron.Collections.Generic;
+
+/// <summary>
+/// A simple container for singleton objects.
+/// </summary>
+/// <typeparam name="T">Yhe <see cref="Type"/> of the object</typeparam>
+public static class Singleton<T> where T : new()
+{
+    #region Objects and variables
+
+    private static ConcurrentDictionary<Type, T> _instances = new ConcurrentDictionary<Type, T>();
+
+    #endregion
+
+    #region Properties
+
+    /// <summary>
+    /// Returns the instance of the given type.
+    /// </summary>
+    public static T Instance => _instances.GetOrAdd(typeof(T), (t) => new T());
+
+    #endregion
+}
